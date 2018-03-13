256bf97d0e99c948abc2d49edd5c886ebdcdd01a
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-03-13 11:21:01 -0700

    Firebase is added to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAA3xgPKeueqLMr35h0A356kdx-upD7pWQ",
+  authDomain: "bloc-chat-react-fb1e9.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-fb1e9.firebaseio.com",
+  projectId: "bloc-chat-react-fb1e9",
+  storageBucket: "bloc-chat-react-fb1e9.appspot.com",
+  messagingSenderId: "987209014782"
+};
+firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
